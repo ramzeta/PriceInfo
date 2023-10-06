399f1d37405dddc9e93b7fff652baf2351e65c92
ramzeta <ramiroperezsanz13@gmail.com>
2023-10-06 02:17:29 +0200

    feat: add PDF amd delete id testing

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -11,8 +11,8 @@ INSERT INTO PRODUCT (PRODUCT_ID, PRODUCT_NAME) VALUES (4, 'Surface Laptop');
 INSERT INTO PRODUCT (PRODUCT_ID, PRODUCT_NAME) VALUES (5, 'Dell XPS 13');
 INSERT INTO PRODUCT (PRODUCT_ID, PRODUCT_NAME) VALUES (35455, 'Prueba');
 
-INSERT INTO PRICE_INFO (priceInfoId, id, brand_id, start_date, end_date, price_list, product_id, priority, price, curr) VALUES
-(1, 1, 1, '2020-06-14 00:00:00.0', '2020-12-31 23:59:59.0', 1, 35455, 0, 35.50, 'EUR'),
-(2, 2, 1, '2020-06-14 15:00:00.0', '2020-06-14 18:30:00.0', 2, 35455, 1, 25.45, 'EUR'),
-(3, 3, 1, '2020-06-15 00:00:00.0', '2020-06-15 11:00:00.0', 3, 35455, 1, 30.50, 'EUR'),
-(4, 4, 1, '2020-06-15 16:00:00.0', '2020-12-31 23:59:59.0', 4, 35455, 1, 38.95, 'EUR');
+INSERT INTO PRICE_INFO (id, brand_id, start_date, end_date, price_list, product_id, priority, price, curr) VALUES
+( 1, 1, '2020-06-14 00:00:00.0', '2020-12-31 23:59:59.0', 1, 35455, 0, 35.50, 'EUR'),
+( 2, 1, '2020-06-14 15:00:00.0', '2020-06-14 18:30:00.0', 2, 35455, 1, 25.45, 'EUR'),
+( 3, 1, '2020-06-15 00:00:00.0', '2020-06-15 11:00:00.0', 3, 35455, 1, 30.50, 'EUR'),
+( 4, 1, '2020-06-15 16:00:00.0', '2020-12-31 23:59:59.0', 4, 35455, 1, 38.95, 'EUR');
